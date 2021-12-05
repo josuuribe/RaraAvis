fee524b6196d68e1328f0f23bbd00c80f8a63b09
Josu Uribe <josu.uribe@raraavis.guru>
2021-12-05 14:01:43 +0000

    Fixing matpolotlib

--- jupyter/ai-jupyterlab-build.sh
+++ jupyter/ai-jupyterlab-build.sh
@@ -1,2 +1,6 @@
-docker build -t joursain/rpi-jupyterlab-ai:buster -f Dockerfile.ai-jupyterlab .
+rm ai.out 2>/dev/null
+nohup docker build -t joursain/rpi-jupyterlab-ai:buster -f Dockerfile.ai-jupyterlab . > ai.out &
+sleep 10
+tail -fn 20 ai.out
+
 
