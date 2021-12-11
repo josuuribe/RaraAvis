4124d0f3272f9cfee0453c88ea0c822104081b52
Josu Uribe <josu.uribe@raraavis.guru>
2021-12-11 18:36:53 +0000

    Fixed jupyterlab ai

--- jupyter/ai-jupyterlab-run.sh
+++
@@ -1,8 +0,0 @@
-docker container run \
---init \
---name jupyterlab-ai \
---publish 8888:8888 \
---detach \
---volume /home/pi/jupyterlab:/home/jupyter/notebooks \
-joursain/rpi-jupyterlab-ai:buster
-
