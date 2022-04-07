97c415a1f253572abe916e1629b9d29c1dc0c57d
Josu Uribe <josu.uribe@raraavis.guru>
2022-04-07 19:38:08 +0100

    ai bullseye

--- jupyter/jupyterlab-build.sh
+++ jupyter/jupyterlab-build.sh
@@ -1,5 +1,5 @@
 rm jupyterlab.out 2>/dev/null
-nohup docker build -t joursain/rpi-jupyterlab:buster -f Dockerfile.jupyterlab . > jupyterlab.out &
+nohup docker build -t joursain/rpi-jupyterlab:bullseye -f Dockerfile.jupyterlab . > jupyterlab.out &
 sleep 10
 tail -fn 20 jupyterlab.out
 
